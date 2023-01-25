b3e3681614700c33b046fa496b046ba785df01e1
Viet Huy Vu <vv761622@ohio.edu>
2023-01-24 19:45:34 -0500

    add git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Viet Huy Vu
+ *        Date: Jan 24th, 2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
